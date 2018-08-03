68ef83ee087a68858a895318ce03add2618c161d
bbharned <bbharned@gmail.com>
2018-08-03 11:02:42 -0400

    initial user table and validations

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+	validates :firstname, presence: true, length: { minimum: 2, maximum: 20 }
+	validates :lastname, presence: true, length: { minimum: 2, maximum: 25 }
+	validates :email, presence: true, length: { minimum: 2, maximum: 40 }
+	validates :company, presence: true, length: { minimum: 3, maximum: 50 }
+	validates :admin, presence: true
+
+
+
+end
