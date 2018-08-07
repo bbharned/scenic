77b8f4e2f1c736272f9dca63e01c5b56692a7f27
bbharned <bbharned@gmail.com>
2018-08-07 13:22:15 -0400

    added partials and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
 
 
+def index
+	@users = User.all
+end
 
 def new 
 	@user = User.new
@@ -36,12 +39,16 @@ def show
 	@user = User.find(params[:id])
 end
 
-
-
-
-def index
-	@users = User.all
+def destroy
+	@user = User.find(params[:id])
+	@user.destroy
+	flash[:notice] = "User was successfully deleted"
+	redirect_to users_path
 end
+
+
+
+
 
 private
 
