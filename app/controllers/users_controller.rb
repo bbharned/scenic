e5c3aa908713ecca26a215b76575011b5add79f8
bbharned <bbharned@gmail.com>
2018-09-14 17:20:39 -0400

    added password confirmation to user CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	before_action :set_user, only: [:edit, :update, :show, :destroy]
+	#before_action :check_password, only: [:update, :create]
 
 def index
 	@users = User.all
@@ -13,25 +14,32 @@ def edit
 	
 end
 
+
 def create
 	#render plain: params[:user].inspect
+	#if :check_password == true
 	@user = User.new(user_params)
-	if @user.save
-		flash[:success] = "User was successfully created"
-		redirect_to users_path
-	else
-		render 'new'
-	end	
+		if @user.save
+			flash[:success] = "User was successfully created"
+			redirect_to users_path
+		else
+			render 'new'
+		end	
+	#else
+		#render'new'
+	#end
 end
 
-def update
+
+def update	
 	if @user.update(user_params)
 		flash[:success] = "User was successfully updated"
 		redirect_to users_path
 	else
 		render 'edit'
-	end	
+	end		
 end
+
 
 def show
 	
@@ -53,8 +61,17 @@ private
 		@user = User.find(params[:id])
 	end
 
+	# def check_password
+	# 	if params[:password] === params[:password2]
+	# 		return true
+	# 	else
+	# 		flash[:danger] = "passwords do not match"
+	# 		return false
+	# 	end
+	# end
+
 	def user_params
-		params.require(:user).permit(:firstname, :lastname, :email, :company)
+		params.require(:user).permit(:firstname, :lastname, :email, :company, :password, :password_confirmation)
 	end
 
 
