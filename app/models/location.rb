1c6c1ab506468e8deb09132ba7a873ff9392216d
bbharned <bbharned@gmail.com>
2018-09-21 11:07:42 -0400

    added validations for locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,11 @@
 class Location < ApplicationRecord
 	has_many :orders
+
+	validates :scl_ref, presence: true
+	validates :name, presence: true, length: { minimum: 2, maximum: 50}
+	validates :sc_type, presence: true
+	validates :address, presence: true
+
+
+	
 end
