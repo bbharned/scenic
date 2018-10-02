215e1484c0bb141401f022fdd76f312b1c9120a1
bbharned <bbharned@gmail.com>
2018-10-02 12:45:20 -0400

    added validations to contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,15 +1,14 @@
 class Contact < ApplicationRecord #ActiveRecord::Base
-	#validates :firstname, presence: true, length: { minimum: 2, maximum: 20 }
-	#validates :lastname, presence: true, length: { minimum: 2, maximum: 25 }
-	#validates :email, presence: true, length: { minimum: 2, maximum: 40 }
-	#validates :company, presence: true, length: { minimum: 3, maximum: 50 }
+	validates :name, presence: true, length: { minimum: 2, maximum: 20 }
+	validates :phone, presence: true, length: { minimum: 2, maximum: 25 }
+	validates :email, presence: true, length: { minimum: 2, maximum: 40 }
+	validates :organization, presence: true, length: { minimum: 3, maximum: 50 }
 
-	#VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
-    #validates :email,  presence: true,  length: { maximum: 105 },
-     #           uniqueness: { case_sensitive: false },
-    #            format: { with: VALID_EMAIL_REGEX }
-    #before_save { self.email = email.downcase }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
+    validates :email,  presence: true,  length: { maximum: 105 },
+                uniqueness: { case_sensitive: false },
+                format: { with: VALID_EMAIL_REGEX }
+    before_save { self.email = email.downcase }
 
-    #has_secure_password
 
 end
